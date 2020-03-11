1f7e0be56ed0d61d03f2b7f52b345a306e58c92c
Karuka3 <karuka.piplup.0327@gmail.com>
2020-03-11 23:29:53 +0900

    Add __init__

--- todoist_modules.py
+++ todoist_modules.py
@@ -1,12 +1,24 @@
 import os
 import re
-from datetime import datetime, timedelta
+import pytz
 from todoist import TodoistAPI
+from datetime import datetime, timedelta
 
-api_token = "565a8c1cc785965dae2950a9cbec280132a4fbe7"
-api = TodoistAPI(api_token)
-api.sync()
 
-project1 = api.projects.add('テストプロジェクト')
-task1 = api.items.add('テストタスク')
-api.commit()
+class TodoistItems:
+    def __init__(self, token):
+        self.weekdays = {
+            0: ['mon', '月曜'],
+            1: ['tue', '火曜'],
+            2: ['wed', '水曜'],
+            3: ['thu', '木曜'],
+            4: ['fri', '金曜'],
+            5: ['sat', '土曜'],
+            6: ['sun', '日曜'],
+        }
+        self.api_token = token
+        self.api = TodoistAPI(self.api_token)
+        self.api.sync()
+
+
+# api_token = "565a8c1cc785965dae2950a9cbec280132a4fbe7"
