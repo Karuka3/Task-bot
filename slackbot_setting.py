501d29c3c01ca2099f6811560279459fbeba107f
Karuka3 <karuka.piplup.0327@gmail.com>
2020-03-12 00:06:28 +0900

    Add setting

---
+++ slackbot_setting.py
@@ -0,0 +1,12 @@
+import os
+
+API_TOKEN = os.environ["BOT_API_TOKEN"]
+
+# 知らない言葉を聞いた時のデフォルトの応答
+DEFAULT_REPLY = "その言葉の意味は知りません"
+
+# 外部ファイルを読み込み
+PLUGINS = [
+    'slackbot.plugins',
+    'bot_module',
+]
