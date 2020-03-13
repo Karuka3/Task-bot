b6e24e32d8a1bd3734d79d866d485cd0551ba848
Karuka3 <karuka.piplup.0327@gmail.com>
2020-03-13 14:54:58 +0900

    Add auto_chat.py

---
+++ auto_chat.py
@@ -0,0 +1,14 @@
+import os
+from slacker import Slacker
+from todoist_modules import TodoistItems
+import bot_modules as bot
+
+
+def main():
+    task = bot.get_task(date="今日")
+    slack = Slacker(os.getenv("HUBOT_API_TOKEN"))
+    slack.chat.post_message(channel='to-do', text=task, as_user=True)
+
+
+if __name__ == "__main__":
+    main()
